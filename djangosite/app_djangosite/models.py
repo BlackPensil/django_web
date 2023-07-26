411c1991b3a951057f339b971ce3e372b5dd487b
BlackPensil <chtoblyat000@gmail.com>
2023-07-26 21:43:38 +0500

    update 3.0 Advertisement

--- djangosite/app_djangosite/models.py
+++ djangosite/app_djangosite/models.py
@@ -1,3 +1,43 @@
-from django.db import models
-
 # Create your models here.
+# Advertisement:
+# title
+# description
+# price
+# auction
+# created_at
+# updated_at
+from django.db import models
+class Advertisement(models.Model):
+
+    title = models.CharField(
+        max_length=100,
+        verbose_name='Название',
+    )
+
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+
+    price = models.DecimalField(
+        verbose_name='Цена',
+        max_digits=10,
+        decimal_places=2
+    )
+
+    auction = models.BooleanField(
+        verbose_name='Торг',
+        default=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата редактирования'
+    )
+
+
+
