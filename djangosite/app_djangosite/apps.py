c83cd4740754555051adcbac17080e8aebb3e6f0
BlackPensil <chtoblyat000@gmail.com>
2023-08-05 22:21:14 +0500

    добавление update_date и изменение apps.py

--- djangosite/app_djangosite/apps.py
+++ djangosite/app_djangosite/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AppDjangositeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_djangosite'
+    verbose_name = 'Объявления'
